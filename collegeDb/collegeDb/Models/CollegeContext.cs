71aa66d864e53dce9fa1e40b193b9cc56f0ca6f1
RaziyaShajeer <Rasiyaaitrich@gmail.com>
2025-03-20 12:01:21 +0530

    message

---
+++ collegeDb/collegeDb/Models/CollegeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collegeDb.Models
+{
+	internal class CollegeContext:DbContext
+	{
+		public DbSet<student> students { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=DESKTOP-PBRNQVI;Initial Catalog=College;Integrated Security=True;Trust Server Certificate=True");
+		}
+	}
+}
---
+++ collegeDb/collegeDb/Models/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collegeDb.Models
+{
+	internal class student
+	{
+		public int Id { get; set; }	
+		public string Name { get; set; }
+		public string Addresss { get; set; }
+		public string City { get; set; }
+	}
+}
