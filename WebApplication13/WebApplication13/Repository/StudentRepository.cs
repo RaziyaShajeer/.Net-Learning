0536f61ab2442c108f1bb704fe17545206a23da4
farhasalman <farhasalman1989@gmail.com>
2025-04-10 15:27:14 +0530

    mysample

---
+++ WebApplication13/WebApplication13/Repository/StudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication13.Interface;
+using WebApplication13.Models;
+
+namespace WebApplication13.Repository
+{
+    public class StudentRepository : IStudentrepository
+    {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        public List<Student> GetStudents()
+        {
+            var std= _context.Students.ToList();
+            return std;
+        }
+
+        public bool AddStudent(Student student)
+        {
+            _context.Students.Add(student); 
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
