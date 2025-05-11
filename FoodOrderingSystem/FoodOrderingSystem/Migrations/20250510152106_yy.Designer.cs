34cd06f5549ac3f11b0513f160197989261ddc55
RaziyaShajeer <rasiyaaitrich@gmail.com>
2025-05-11 22:10:58 +0530

    hh

--- FoodOrderingSystem/FoodOrderingSystem/Migrations/20250510152106_yy.Designer.cs
+++ FoodOrderingSystem/FoodOrderingSystem/Migrations/20250510152106_yy.Designer.cs
@@ -4,6 +4,7 @@ using FoodOrderingSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FoodOrderingSystem.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250510152106_yy")]
+    partial class yy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
