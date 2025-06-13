0df0ba6528fdb39621cc1c74c8ebc98e7e9fc176
farhasalman <farhasalman1989@gmail.com>
2025-06-13 20:51:04 +0530

    sss

---
+++ MainProject/FoodOrderingSystem/FoodOrderingSystem/Migrations/20250524152509_locationdpchange.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrderingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationdpchange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationID",
+                table: "UserDTO");
+
+            migrationBuilder.RenameColumn(
+                name: "DishImages",
+                table: "Dish",
+                newName: "DishImage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationName",
+                table: "UserDTO",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "MyUser",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationName",
+                table: "MyUser",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "DishDTO",
+                columns: table => new
+                {
+                    DishName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    RestaurantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DishDTO");
+
+            migrationBuilder.DropColumn(
+                name: "LocationName",
+                table: "UserDTO");
+
+            migrationBuilder.DropColumn(
+                name: "LocationName",
+                table: "MyUser");
+
+            migrationBuilder.RenameColumn(
+                name: "DishImage",
+                table: "Dish",
+                newName: "DishImages");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LocationID",
+                table: "UserDTO",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "MyUser",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
---
+++ MainProject/FoodOrderingSystem/FoodOrderingSystem/Migrations/20250529160412_locationid.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrderingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MyUser_Location",
+                table: "MyUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK__MyUser__1788CC4C3F1B5C0A",
+                table: "MyUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MyUser_LocationId",
+                table: "MyUser");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "MyUser");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "DishDTO");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyUser",
+                table: "MyUser",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyUser",
+                table: "MyUser");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LocationId",
+                table: "MyUser",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RestaurantId",
+                table: "DishDTO",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK__MyUser__1788CC4C3F1B5C0A",
+                table: "MyUser",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MyUser_LocationId",
+                table: "MyUser",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MyUser_Location",
+                table: "MyUser",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId");
+        }
+    }
+}
