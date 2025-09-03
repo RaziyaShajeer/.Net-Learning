8d719d18a16ec4bdde58a95201d7cbed1aa1949a
RaziyaShajeer <Rasiyaaitrich@gmail.com>
2025-09-03 17:42:08 +0530

    d

--- MainProject/FoodOrderingSystem/FoodOrderingSystem/Models/MyDbContext.cs
+++ MainProject/FoodOrderingSystem/FoodOrderingSystem/Models/MyDbContext.cs
@@ -33,107 +33,16 @@ public partial class MyDbContext : DbContext
     public virtual DbSet<RestaurantAdmin> RestaurantAdmins { get; set; }
 
     public virtual DbSet<RestaurantProfile> RestaurantProfiles { get; set; }
+    public virtual DbSet<Category> Category { get; set; }   
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-PBRNQVI;Initial Catalog=FoodOrdering;Integrated Security=True;Trust Server Certificate=True");
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-PBRNQVI;Initial Catalog=FoodOrdering1;Integrated Security=True;Trust Server Certificate=True");
+	}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Cart>(entity =>
-        {
-            entity.HasKey(e => e.CartId).HasName("PK__Cart__51BCD7B78862E83E");
+public DbSet<FoodOrderingSystem.DTO.CategoryDTO> CategoryDTO { get; set; } = default!;
 
-            entity.Property(e => e.CartId).ValueGeneratedNever();
 
-            entity.HasOne(d => d.User).WithMany(p => p.Carts)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Cart_User");
-        });
 
-        modelBuilder.Entity<Cartitem>(entity =>
-        {
-            entity.HasKey(e => e.CartItemId).HasName("PK__Cartitem__488B0B0A3FD7C69B");
 
-            entity.Property(e => e.CartItemId).ValueGeneratedNever();
-
-            entity.HasOne(d => d.Dish).WithMany(p => p.Cartitems)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Cartitem_Dish");
-        });
-
-        modelBuilder.Entity<Dish>(entity =>
-        {
-            entity.HasKey(e => e.DishId).HasName("PK__Dish__18834F501F9461B5");
-
-            entity.Property(e => e.DishId).ValueGeneratedNever();
-
-            entity.HasOne(d => d.Restaurant).WithMany(p => p.Dishes)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Dish_RestaurantProfile");
-        });
-
-        modelBuilder.Entity<Location>(entity =>
-        {
-            entity.HasKey(e => e.LocationId).HasName("PK__Location__E7FEA4975324939F");
-
-            entity.Property(e => e.LocationId).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<MyOrder>(entity =>
-        {
-            entity.HasKey(e => e.OrderId).HasName("PK__MyOrder__C3905BCFFB2199AF");
-
-            entity.Property(e => e.OrderId).ValueGeneratedNever();
-
-            entity.HasOne(d => d.User).WithMany(p => p.MyOrders)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Order_User");
-        });
-
-       
-        modelBuilder.Entity<OrderItem>(entity =>
-        {
-            entity.HasKey(e => e.OrderItemId).HasName("PK__OrderIte__57ED06813226F653");
-
-            entity.Property(e => e.OrderItemId).ValueGeneratedNever();
-
-            entity.HasOne(d => d.OrderItemNavigation).WithOne(p => p.OrderItem)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_OrderItem_Order");
-        });
-
-        modelBuilder.Entity<RestaurantAdmin>(entity =>
-        {
-            entity.HasKey(e => e.RestaurantAdminId).HasName("PK__Restaura__F5EA8F876F0D6D3A");
-
-            entity.Property(e => e.RestaurantAdminId).ValueGeneratedNever();
-
-            entity.HasOne(d => d.Restaurant).WithMany(p => p.RestaurantAdmins)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_RestaurantAdmin_RestaurantProfile");
-        });
-
-        modelBuilder.Entity<UserDTO>().HasNoKey();
-        modelBuilder.Entity<DishDTO>().HasNoKey();  
-
-        //modelBuilder.Entity<RestaurantProfile>(entity =>
-        //{
-        //    entity.HasKey(e => e.RestaurantId).HasName("PK__Restaura__87454C95236D507F");
-
-        //    entity.Property(e => e.RestaurantId).ValueGeneratedNever();
-
-        //    entity.HasOne(d => d.Location).WithMany(p => p.RestaurantProfiles)
-        //        .OnDelete(DeleteBehavior.ClientSetNull)
-        //        .HasConstraintName("FK_RestaurantProfile_Location");
-        //});
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-
-public DbSet<FoodOrderingSystem.DTO.UserDTO> UserDTO { get; set; } = default!;
-
-public DbSet<FoodOrderingSystem.DTO.DishDTO> DishDTO { get; set; } = default!;
 }
